8c71338a37350637c0ce0e4754777a04e51333cb
anshIA09 <Anish.Pawaskar@interactiveavenues.com>
2026-02-03 16:04:13 +0530

    feat: update config to fix netscope issue

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  experimental: {
+    turbopackUseSystemTlsCerts: true, // NOTE: Add this because we were facing font issue
+  },
 };
 
 export default nextConfig;
